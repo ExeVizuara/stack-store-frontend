23daa4dc27879e662443967e7c4cb3047536c2aa
ExeVizuara <vizuara01@gmail.com>
2024-09-11 00:27:01 -0300

    corrections

--- src/components/Sale/SellItem.jsx
+++ src/components/Sale/SellItem.jsx
@@ -1,7 +1,7 @@
 export function SellItem({ name, price, quantity }) {
     return (
         <ul className="flex flex-row justify-between">
-            <li className="w-1/2"><h5>{name}</h5></li>
+            <li className="w-1/3"><h5>{name}</h5></li>
             <li className=""><h5>{quantity}</h5></li>
             <li className=""><span>${ price }</span></li>
         </ul>
--- src/components/shared/TitleSection.jsx
+++ src/components/shared/TitleSection.jsx
@@ -7,7 +7,7 @@ export function TitleSection() {
 
     return (
 
-        <div className="flex flex-row justify-around">
+        <div className="flex flex-row justify-around bg-gray-900 py-1 mb-4">
             <h1 className="text-2xl text-gray-300">StackStore v1.1</h1>
             <Clock />
         </div>
